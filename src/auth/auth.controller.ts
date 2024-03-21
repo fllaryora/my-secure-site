038779400f2587545c84f017c23a54f39f5afc69
Francisco <fllaryora@gmail.com>
2024-03-21 12:01:40 -0300

    add dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,13 +6,18 @@ import {
     HttpCode,
     HttpStatus,
     Request,
-    UseGuards
+    UseGuards,
+    UnauthorizedException
  } from '@nestjs/common';
 
- import { AuthService } from './auth.service';
+import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { Public } from './decorators/public.decorator';
+import { CreateUserDto } from './dto/UserDto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
 
@@ -24,8 +29,16 @@ export class AuthController {
     @Public() //avoid rejection in case of not loaded
     @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDTO : Record<string, any>){
-        return this.authService.signIn(signInDTO.userName, signInDTO.password);
+    @ApiOperation({summary: 'Perform the log in'})
+    //signIn(@Body() signInDTO : Record<string, any>) {
+    signIn(@Body() createUserDto: CreateUserDto){
+        //console.log(signInDTO.userName);
+        if(createUserDto.userName == undefined || createUserDto.password == undefined) {
+        //if(signInDTO.userName == undefined || signInDTO.password == undefined) {
+            throw new UnauthorizedException();
+        }
+        return this.authService.signIn(createUserDto.userName, createUserDto.password);
+        //return this.authService.signIn(signInDTO.userName, signInDTO.password);
     }
     //Funciona con pepe
     //curl -X POST http://localhost:3000/auth/login -d '{"userName": "John", "password": "changeme"}' -H "Content-Type: application/json"
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//npm install --save @nestjs/swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Cats sample')
+  .setDescription('The cats api description')
+  .setVersion('1.0')
+  //.addTag('cats')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(
+    app, config
+  );
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
